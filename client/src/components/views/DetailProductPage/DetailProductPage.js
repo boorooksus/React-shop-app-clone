c42a34e82967a41bf3060c6127be06379ba4653c
boorooksus <boorooksus@gmail.com>
2021-04-23 11:32:23 +0900

    product Image 컴포넌트

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,25 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
+import { Row, Col } from 'antd';
 
 function DetailProductPage(props) {
-    
+
     const productId = props.match.params.productId
+
+    const [Product, setProduct] = useState({})
 
     useEffect(() => {
         // db에서 하나만 가져오므로 'type=single'
         axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then(response => {
                 if(response.data.success){
-                    console.log('response.data', response.data)
+                    console.log('response.data:', response.data)
                 } else{
-                    alert('상품 정보 실패')
+                    alert('실패')
                 }
+                setProduct(response.data.product[0])
+                console.log('response.data[0]: ',response.data[0] )
             })
+            .catch(err => alert(err))
     }, [])
 
-    return(
-        <div>
-            detailProductPage
+
+
+    return (
+        <div style={{ width: '100%', padding: '3rem 4rem' }}>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h1>{Product.title}</h1>
+            </div>
+
+            <br />
+
+            <Row gutter={[16, 16]} >
+                <Col lg={12} sm={24}>
+                    {/* ProductImage */}
+                    {/* 프로덕트 모던 정보를 property로 내려준다 -> ProductImage에서 컨트롤 가능 */}
+                    <ProductImage detail={Product} />
+                </Col>
+                <Col lg={12} sm={24}>
+                    {/* ProductInfo */}
+                    <ProductInfo detail={Product} />
+                </Col>
+            </Row>
+
+
+
+
+
         </div>
     )
 }
---
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import ImageGallery from 'react-image-gallery';
+
+function ProductImage(props) {
+
+    const [Images, setImages] = useState([])
+
+    useEffect(() => {
+
+        if (props.detail.images && props.detail.images.length > 0) {
+            let images = []
+
+            props.detail.images.map(item => {
+                images.push({
+                    original: `http://localhost:5000/${item}`,
+                    thumbnail: `http://localhost:5000/${item}`
+                })
+            })
+            setImages(images)
+        }
+        // [props.detail]: props.detail의 값이 바뀔 때마다 라이프 사이클 한 번 더 실행한다는 의미
+    }, [props.detail])
+
+    return (
+        <div>
+            <ImageGallery items={Images} />
+        </div>
+    )
+}
+
+export default ProductImage
